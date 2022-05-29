9777b66e72709c0e3a86eb95c094f026b923741b
Felipe Bazares <80702279+Stormimation@users.noreply.github.com>
2022-05-29 15:37:43 -0400

    Add files via upload

---
+++ Proyecto_final-main/principal/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import Medico, UsuarioP3, Hora, Dia, Mes, Agendar, UsuarioM, UsuarioS, Agendar5
+
+# Register your models here.
+
+
+
+class MedicoAdmin(admin.ModelAdmin):                                              #Quitar Simbolo de comentario solo para fines administrativos
+    list_display=("idMedico","nombreMedico", "apellidoMedico","especialidad")
+
+class UsuarioPAdmin(admin.ModelAdmin):
+    list_display=("rut","email","nombre","edad","password")   
+
+class UsuarioMAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","password")
+
+class UsuarioSAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","password")  
+
+class Agendar5Admin(admin.ModelAdmin):
+    list_display=("idHora", "dia", "mes", "hora", "nombre", "medico")    
+           
+
+#class DiaAdmin(admin.ModelAdmin):            #Quitar Simbolo de comentario solo para fines administrativos
+    #list_display=["dia"]
+
+#class HoraAdmin(admin.ModelAdmin):
+    #list_display=["hora"]
+
+#class MesAdmin(admin.ModelAdmin):
+    #list_display=["mes"]
+
+    
+
+
+admin.site.register(UsuarioP3,UsuarioPAdmin)
+admin.site.register(UsuarioM, UsuarioMAdmin)
+admin.site.register(UsuarioS, UsuarioSAdmin)
+#admin.site.register(Hora,HoraAdmin)              #Quitar Simbolo de comentario solo para fines administrativos
+#admin.site.register(Dia,DiaAdmin)
+#admin.site.register(Mes,MesAdmin)
+admin.site.register(Agendar)
+admin.site.register(Agendar5, Agendar5Admin)
+admin.site.register(Medico, MedicoAdmin)
